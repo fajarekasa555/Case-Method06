1288c86e712c020838414d8c5f82292464314757
uotavia <uotavia@gmail.com>
2025-04-18 22:43:00 +0700

    Update: Mahasiswa Data

--- MahasiswaData.java
+++ MahasiswaData.java
@@ -1,3 +1,14 @@
 public class MahasiswaData {
-    
+    String nim, nama, prodi;
+
+    MahasiswaData(String nim, String nama, String prodi){
+        this.nim =nim;
+        this.nama =nama;
+        this.prodi =prodi;
+    }
+    void tampilInfo(){
+        System.out.println("NIM   :"+this.nim);
+        System.out.println("Nama  :"+this.nama);
+        System.out.println("Prodi :"+this.prodi);
+    }    
 }
